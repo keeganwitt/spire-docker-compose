8cbde9705eb88eb8ad96f895723a5fd478fa311c
Keegan Witt <keeganwitt@gmail.com>
2024-09-27 12:21:22 -0400

    Sleep after server start

--- start.ps1
+++ start.ps1
@@ -1,6 +1,8 @@
 Write-Host "Starting SPIRE Server"
 docker-compose up -d spire-server
 
+Start-Sleep -Seconds 5
+
 Write-Host "bootstrapping SPIRE Agent..."
 docker-compose exec -T spire-server /opt/spire/bin/spire-server bundle show > spire/agent/bootstrap.crt
 
--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ set -o errexit -o nounset -o pipefail
 echo "Starting SPIRE Server"
 docker-compose up -d spire-server
 
+sleep 5s
+
 echo "bootstrapping SPIRE Agent..."
 docker-compose exec -T spire-server /opt/spire/bin/spire-server bundle show > spire/agent/bootstrap.crt
 
