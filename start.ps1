2d1b959ddca7806d5cca709ae2c3b309c1fb3d4f
Keegan Witt <keeganwitt@gmail.com>
2025-02-18 23:33:04 -0500

    Move entry creation into Docker Compose

--- start.ps1
+++ start.ps1
@@ -2,11 +2,7 @@ Write-Host "Starting SPIRE server .."
 docker-compose up -d --wait spire-server
 
 Write-Host "Creating entries..."
-docker-compose exec -T spire-server /opt/spire/bin/spire-server entry create `
-    -selector docker:label:com.docker.compose.service:spiffe-helper `
-    -spiffeID spiffe://example.org/workload/spiffe-helper `
-    -parentID spiffe://example.org/spire/agent/x509pop/02b8e7713492fdf93d43369e9c6f50d28bef9fa8
-#    -parentID spiffe://example.org/node/agent
+docker-compose up -d --wait create-entries
 
 # commented out because using insecure_bootstrap
 #Write-Host "Bootstrapping SPIRE Agent..."
